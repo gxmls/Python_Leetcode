c1190a87a18901ee47d5fd6dd024e2a56765aac0
gxmls <54612072+gxmls@users.noreply.github.com>
2021-05-21 10:24:58 +0800

    Add files via upload

---
+++ 1103.py
@@ -0,0 +1,47 @@
+'''
+排排坐，分糖果。
+我们买了一些糖果 candies，打算把它们分给排好队的 n = num_people 个小朋友。
+给第一个小朋友 1 颗糖果，第二个小朋友 2 颗，依此类推，直到给最后一个小朋友 n 颗糖果。
+然后，我们再回到队伍的起点，给第一个小朋友 n + 1 颗糖果，第二个小朋友 n + 2 颗，依此类推，直到给最后一个小朋友 2 * n 颗糖果。
+重复上述过程（每次都比上一次多给出一颗糖果，当到达队伍终点后再次从队伍起点开始），直到我们分完所有的糖果。注意，就算我们手中的剩下糖果数不够（不比前一次发出的糖果多），这些糖果也会全部发给当前的小朋友。
+返回一个长度为 num_people、元素之和为 candies 的数组，以表示糖果的最终分发情况（即 ans[i] 表示第 i 个小朋友分到的糖果数）。
+示例 1：
+输入：candies = 7, num_people = 4
+输出：[1,2,3,1]
+解释：
+第一次，ans[0] += 1，数组变为 [1,0,0,0]。
+第二次，ans[1] += 2，数组变为 [1,2,0,0]。
+第三次，ans[2] += 3，数组变为 [1,2,3,0]。
+第四次，ans[3] += 1（因为此时只剩下 1 颗糖果），最终数组变为 [1,2,3,1]。
+示例 2：
+输入：candies = 10, num_people = 3
+输出：[5,2,3]
+解释：
+第一次，ans[0] += 1，数组变为 [1,0,0]。
+第二次，ans[1] += 2，数组变为 [1,2,0]。
+第三次，ans[2] += 3，数组变为 [1,2,3]。
+第四次，ans[0] += 4，最终数组变为 [5,2,3]。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/distribute-candies-to-people
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        import math
+        ls=[0 for i in range(num_people)] #初始化一个数组，表示每个人手里的糖果数
+        ro=math.ceil(candies/num_people) #分了ro轮糖果
+        for r in range(ro):
+            for i in range(num_people):
+                candies-=(i+1+num_people*r) #判断糖果数是否足够分给下一个人
+                if candies>=0: #如果剩余糖果可以分给下一个人
+                    ls[i]+=(i+1+num_people*r) #分糖果给当前的人
+                    continue
+                else:
+                    ls[i]+=candies+(i+1+num_people*r) #如果糖果数不足够分给下一个人，把剩下所有糖果分给当前的人
+                    break
+            else:
+                continue
+            break #跳出整个循环
+        return ls
+
