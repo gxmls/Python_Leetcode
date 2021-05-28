8a0ec7ad56e7328b8ca644c55c81935a4bc24365
gxmls <54612072+gxmls@users.noreply.github.com>
2021-05-28 17:53:17 +0800

    Add files via upload

---
+++ 287.py
@@ -0,0 +1,30 @@
+'''
+给定一个包含 n + 1 个整数的数组 nums ，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+假设 nums 只有 一个重复的整数 ，找出 这个重复的数 。
+你设计的解决方案必须不修改数组 nums 且只用常量级 O(1) 的额外空间。
+示例 1：
+输入：nums = [1,3,4,2,2]
+输出：2
+示例 2：
+输入：nums = [3,1,3,4,2]
+输出：3
+示例 3：
+输入：nums = [1,1]
+输出：1
+示例 4：
+输入：nums = [1,1,2]
+输出：1
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-the-duplicate-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        from collections import Counter
+        dn=dict(Counter(nums))
+        ls=[key for key,value in dn.items() if value > 1] #用Counter()找到重复元素
+        for item in ls:
+            return item
+
+#如果用set()去重遍历set(),再通过nums.count()判断是否重复会运算超时
